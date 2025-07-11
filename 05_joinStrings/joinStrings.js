852abc37267c28eebe55643ee70c5e8b7164d922
adrianobawan <167875657+adrianobwn@users.noreply.github.com>
2025-07-11 19:00:22 +0700

    updated

--- 05_joinStrings/joinStrings.js
+++ 05_joinStrings/joinStrings.js
@@ -7,9 +7,16 @@
  The greeting should read: "Hello! My name is Carlos Stevenson and I am 18 years old."
 */
 
+const { c } = require("../04_mathEquations/mathEquations");
+
 // Add your code right below, good luck!
-
-
+const firstName = 'Carlos'; // Your first name
+const lastName = 'Stevenson'; // Your last name
+const thisYear = 1965; // The current year
+const birthYear = 1947; // Your birth year
+const greeting = `Hello! My name is ${firstName} ${lastName} and I am ${thisYear - birthYear} years old.`; // The greeting
+const fullName = `${firstName} ${lastName}`; // Your full name
+const age = thisYear - birthYear; // Your age
 
 
 
